ef5db2fc5b27b9ffb894901a18d90f8d1dc4aafa
Simonas Mikulis <simonas.mikulis+bitbucket@gmail.com>
2017-04-28 15:26:01 +0300

    Add project files.
    Initial commit

---
+++ RoutingRIP/Graph/Graph.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using RoutingRIP.JSON;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace RoutingRIP
+{
+    class Graph
+    {
+        public List<GraphNode> Nodes = new List<GraphNode>();
+
+        public Graph() { }
+
+        public bool LoadFromJSON(string jsonString)
+        {
+            try
+            {
+                GraphJSON nodes = JsonConvert.DeserializeObject<GraphJSON>(jsonString);
+                foreach(var node in nodes.NodeNames)
+                {
+                    Nodes.Add(new GraphNode { Name = node.Name });
+                }
+                foreach(var connection in nodes.Connections)
+                {
+                    GraphNode from = Nodes.Find(x => x.Name == connection.From);
+                    GraphNode to = Nodes.Find(x => x.Name == connection.To);
+                    from.Connections.Add(new NodeLink { To = to, Weight = connection.Weight });
+                    to.Connections.Add(new NodeLink { To = from, Weight = connection.Weight });
+                }
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public string ToGraphVizString()
+        {
+            List<Link> usedLinks = new List<Link>();
+            StringBuilder graphviz = new StringBuilder();
+            graphviz.Append("graph Nodes {");
+            foreach(var node in Nodes)
+            {
+                foreach(var connection in node.Connections)
+                {
+                    if (!usedLinks.Exists(x => (x.From == connection.To.Name && x.To == node.Name)) )
+                    {
+                        graphviz.Append(Environment.NewLine + node.Name + "--" + connection.To.Name);
+                        usedLinks.Add(new Link { From = node.Name, To = connection.To.Name });
+                    }                    
+                }
+            }
+            graphviz.Append('}');
+            return graphviz.ToString();
+        }
+    }
+}
---
+++ RoutingRIP/Graph/GraphNode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RoutingRIP
+{
+    class GraphNode
+    {
+        public string Name { get; set; }
+
+        public List<NodeLink> Connections = new List<NodeLink>();
+    }
+}
